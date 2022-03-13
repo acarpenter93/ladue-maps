492083eca5acc92ce6a437f7be3753ef73736d7a
Anna Carpenter <acarpenter9393@gmail.com>
2022-03-13 14:19:53 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="logo">
+      <img alt="Ladue logo" src="@/assets/logo.png" />
+    </div>
+    <h1> Welcome to Ladue Horton Watkins High School</h1>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+body {
+  padding: 1rem; 
+ }
+
+#app {
+  background: #F3F3F3; 
+  margin: 0 auto;
+
+  font-weight: normal;
+}
+
+header {
+  align-items:center;
+  background:#2C4AA0;
+  display:flex;
+  color:#FFF;
+  height: 156px;
+}
+h1 {
+  flex:1;
+  text-align:center;
+}
+.logo {
+  align-items:center;
+  background:#243C82;
+  display:flex;
+  height:156px;
+  justify-content: center;
+  width:25%; 
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    map goes here 
+  </main>
+</template>
